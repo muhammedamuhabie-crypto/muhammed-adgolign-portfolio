391f3f301efcb775c996c3e110bd6f07903a6adb
Muhammed Adgolign <muhammed@example.com>
2026-09-21 00:26:19 +0300

    Add XML sitemap

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,18 @@
-from django.contrib import admin
+﻿from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
 from portfolio.views import home, certificate_view, certificate_file_view, certification_view
+from portfolio.sitemaps import PortfolioSitemap
+
+
+sitemaps = {"portfolio": PortfolioSitemap()}
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django-sitemap"),
     path("", home, name="home"),
     path("certificate/<int:certificate_id>/", certificate_view, name="certificate_view"),
     path("certificate/<int:certificate_id>/file/", certificate_file_view, name="certificate_file_view"),
---
+++ portfolio/sitemaps.py
@@ -0,0 +1,13 @@
+﻿from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+
+
+class PortfolioSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 1.0
+
+    def items(self):
+        return ["home"]
+
+    def location(self, item):
+        return reverse(item)
